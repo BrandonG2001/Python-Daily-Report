aa8540ad4d1c8fb1b59f69ed6f921bc6a039d9bb
Brandon Graeber <Brandon.graeber.01@gmail.com>
2023-11-17 14:44:24 -0600

    modified:   auto_website_open.py

--- auto_website_open.py
+++ auto_website_open.py
@@ -8,23 +8,13 @@ import asyncio
 # function to open webrowser on any dict
 async def OpenBrowser(url_dict: dict) -> None:
     schema = 'https://'
-    temp_dict = {}
-    # Bypass for certain websites that I know will give 403 response
-    for url_key in url_dict:
-        if url_dict[url_key] == 'chat.openai.com':
-            url = schema + 'chat.openai.com'
-            web.open(url=url)
-        else:
-           temp_dict[url_key] = url_dict[url_key]
-         
-    # get a list of all urls
     all_urls = []
-    for url_key in temp_dict:
-        base_url = temp_dict[url_key]
+    for url_key in url_dict:
+        base_url = url_dict[url_key]
         base_url = base_url.strip()
         url = (schema + base_url).strip()
         all_urls.append(url)
-        
+
 
     # async requests to make it more scalable and faster
     # make sure each website is actually reachable 
@@ -34,17 +24,30 @@ async def OpenBrowser(url_dict: dict) -> None:
 
     for index, response in enumerate(results):
         try:
-            if response.status_code == 200 or (response.status_code==301 or response.status_code==302): # Check if the URL is reachable before browser query 
-                web.open(url=all_urls[index])
-            else:
-                log.warning(f'GET Request Failed (Status Code Not 200) and returned a {response.status_code} status code for {all_urls[index]}')
+            # Check if the URL is reachable before browser query
+            # switch statement based on the status code
+            match response.status_code:
+                # 200 = ALL GOOD
+                case 200: 
+                    web.open(url=all_urls[index])
+                # 301 is old url code -> will be sent to updated url
+                case 301:  
+                    web.open(url=all_urls[index])
+                # 302 is temporary version of code 301
+                case 302:  
+                    web.open(url=all_urls[index])
+                # 403 is = no program code access 
+                case 403:
+                    web.open(url=all_urls[index])
+                # default = Sorry error happened
+                case _:
+                    log.warning(f'GET Request Failed (Status Code Not 200) and returned a {response.status_code} status code for {all_urls[index]}')
         except RequestException as error:
             log.critical(f'HTTP Error occurred, failed to reach URL: {error}')
         except Exception as error:
             log.critical(f'An unexpected error occurred: {error}')
             
-        
-        
+         
 def open_my_websites() -> None:
     personal_urls = {
         'Youtube' : 'www.youtube.com',
@@ -53,6 +56,8 @@ def open_my_websites() -> None:
         'My G-Drive' : 'drive.google.com/drive/u/0/my-drive',
         'My Gmail' : 'mail.google.com/mail/u/0/#inbox',
         'Github' :'www.github.com/BrandonG2001?tab=repositories',
+        'Amazon' : 'www.amazon.com',
+        'Reqzone' : 'reqzone.com'
     }
 
     work_urls = {
@@ -69,3 +74,4 @@ def open_my_websites() -> None:
 if __name__ == '__main__':
     open_my_websites()
     sys.exit()
+    
